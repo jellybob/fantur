64bd09893a9bf23891687509881044553ef0ee00
Jon Wood <jon@blankpad.net>
2016-06-14 20:33:25 +0100

    An actual thing in the app!
    Its just a list of planet names, but its something.

--- Fantur/Fantur.AndroidApp/MainActivity.cs
+++ Fantur/Fantur.AndroidApp/MainActivity.cs
@@ -1,30 +1,24 @@
-﻿using System;
+﻿using System.Linq;
 using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Fantur.Core;
 
 namespace Fantur.AndroidApp
 {
-    [Activity(Label = "Fantur.AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    [Activity(Label = "Fantur", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : ListActivity
     {
-        int count = 1;
+        public Universe Universe;
+        protected string[] EntityNames;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
-
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
-
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            Universe = BigBang.CreateUniverse();
+            EntityNames = Universe.FindAllEntitiesWithComponent(ComponentTypes.Name).Select(e => e.Name).ToArray();
+            ListAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, EntityNames);
         }
     }
 }
